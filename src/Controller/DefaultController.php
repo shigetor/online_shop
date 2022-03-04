963bf656113a832264e4e34c71dfed969cff4b7c
shigetor <sash.koirev.ru@gmail.com>
2022-03-04 17:16:55 +0400

    commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,29 +5,34 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @throws \Exception
-     */
-
-    public function homepage(): Response
-    {
-//        $product = $this->getDoctrine()
-//            ->getRepository(Product::class)
-//            ->find($id);
+//    /**
+//     * @throws \Exception
+//     */
 //
-//        if (!$product) {
-//            throw $this->createNotFoundException(
-//                'No product found for id '.$id
-//            );
-//        }
-
-        return $this->render('default/page.html');
-
-    }
+////    public function homepage(EntityManagerInterface $em, Request $request): Response
+////    {
+////        $cr = $em->getRepository('App:Category');
+////        $categories = $cr->findAll();
+////
+////        return $this->render('default/index.html.twig',[
+////            'categories'=>$categories,
+////        ]);
+////    }
+////
+////    public function category(int $id,EntityManagerInterface $em): Response
+////    {
+////       $category = $em->getRepository('App:Category')->find($id);
+////
+////       return $this->render('default/category.html.twig',[
+////           'category'=>$category,
+////       ]);
+////    }
 }
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordHasherEncoder;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Serializer\Encoder\EncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/registration", name="registration")
+     */
+    public function index(EntityManagerInterface $em,Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $user->setPassword($this->passwordEncoder->hashPassword($user, $user->getPassword()));
+
+            // Set their role
+            $user->setRoles(['ROLE_USER']);
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+//            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
